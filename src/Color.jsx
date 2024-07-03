31c5b5a75f7ce4ce8dfe29af4ff2790ed48239da
Misam-Lakhotara <misamlakhotara@gmail.com>
2024-07-03 16:31:05 +0530

    update dynamic color

--- src/Color.jsx
+++ src/Color.jsx
@@ -2,45 +2,36 @@ import React, { useState, useEffect } from "react";
 
 export default function Color() {
   const [colors, setColors] = useState([]);
-  const [selectedColor, setSelectedColor] = useState(null);
+  const [selectedColor, setSelectedColor] = useState("gray");
 
   useEffect(() => {
     const fetchColors = async () => {
       try {
         const response = await fetch("http://localhost:8080/api/colors");
-        if (!response.ok) {
-          throw new Error("Failed to fetch colors");
-        }
         const data = await response.json();
-        if (!Array.isArray(data.colors)) {
-          throw new Error("Expected an array of colors");
-        }
-        setColors(data.colors); // Set the fetched data into state
+        setColors(data.colors);
       } catch (error) {
         console.error("Error fetching colors:", error);
-        // Handle error state if needed
       }
     };
 
     fetchColors();
-  }, []); // Empty dependency array means this effect runs only once after initial render
+  }, []);
 
   const handleColorClick = (colorName) => {
     setSelectedColor(colorName);
   };
 
   return (
-    <div>
-      <div>
+    <div className=" flex flex-col mt-20 justify-center items-center space-x-10">
+      <div className=" flex flex-row">
         {colors.map((color) => (
           <button
             key={color.id}
             style={{
               backgroundColor: color.value,
-              margin: "5px",
-              padding: "10px",
-              cursor: "pointer",
             }}
+            className=" m-5 p-5"
             onClick={() => handleColorClick(color.label)}
           >
             {color.label}
@@ -48,20 +39,13 @@ export default function Color() {
         ))}
       </div>
       {selectedColor && (
-        <div style={{ marginTop: "20px" }}>
-          <h2>Selected Color: {selectedColor}</h2>
+        <div className=" flex-row mt-20">
           <div
-            style={{
-              width: "100px",
-              height: "100px",
-              backgroundColor: colors.find(
-                (color) => color.label === selectedColor
-              )?.value,
-            }}
+            className="w-96 h-96"
+            style={{ backgroundColor: selectedColor }}
           ></div>
         </div>
       )}
     </div>
   );
 }
-p;
