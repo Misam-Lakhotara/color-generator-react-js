61abfb2d2100d8225a704ba6c79e0c662f717e50
Misam-Lakhotara <misamlakhotara@gmail.com>
2024-07-03 17:57:56 +0530

    add axios

--- src/Color.jsx
+++ src/Color.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function Color() {
   const [colors, setColors] = useState([]);
@@ -7,9 +8,8 @@ export default function Color() {
   useEffect(() => {
     const fetchColors = async () => {
       try {
-        const response = await fetch("http://localhost:8080/api/colors");
-        const data = await response.json();
-        setColors(data.colors);
+        const response = await axios.get("http://localhost:8080/api/colors");
+        setColors(response.data.colors);
       } catch (error) {
         console.error("Error fetching colors:", error);
       }
@@ -23,15 +23,15 @@ export default function Color() {
   };
 
   return (
-    <div className=" flex flex-col mt-20 justify-center items-center space-x-10">
-      <div className=" flex flex-row">
+    <div className="flex flex-col mt-20 justify-center items-center space-x-10">
+      <div className="flex flex-row">
         {colors.map((color) => (
           <button
             key={color.id}
             style={{
               backgroundColor: color.value,
             }}
-            className=" m-5 p-5"
+            className="m-5 p-5"
             onClick={() => handleColorClick(color.label)}
           >
             {color.label}
@@ -39,7 +39,7 @@ export default function Color() {
         ))}
       </div>
       {selectedColor && (
-        <div className=" flex-row mt-20">
+        <div className="flex-row mt-20">
           <div
             className="w-96 h-96"
             style={{ backgroundColor: selectedColor }}
